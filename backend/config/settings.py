1f2198e21c34f509416316b75dcbe12793fd3828
Ishan Bhagat <bhagatishan14@gmail.com>
2026-08-10 11:01:37 +0000

    Configure database via environment variable, default to SQLite

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 
@@ -73,10 +74,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
