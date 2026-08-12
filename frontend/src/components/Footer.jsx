450f5742424cdcdf9f5c4d0772958d413ec8fae2
Ishan Bhagat <bhagatishan14@gmail.com>
2026-08-12 12:32:07 +0000

    Add routing, layout, header, footer, placeholder pages

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Election Management System</p>
+        </footer>
+    );
+}
+
+export default Footer;
